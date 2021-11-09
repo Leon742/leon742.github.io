eb3bba697f3241d82267f00da70210677e621f6a
Leon742 <71738092+Leon742@users.noreply.github.com>
2021-11-08 20:47:53 -0600

    Update sw_aux.js

--- js/sw_aux.js
+++ js/sw_aux.js
@@ -1,13 +1,14 @@
-function actualizarCacheDinamico(dynamicCache,req,res){
+//Almacenamiento dinamico
+function actualizarCacheDinamico(dynamicCache, req, res){
+    
     if(res.ok){
-        return caches.open(dynamicCache).then( cache => {
-            cache.put(req,res.clone());
+        return caches.open(dynamicCache).then(cache=>{
+            cache.put(req, res.clone());
             return res.clone();
         });
-    }else{
-        // return fetch(event.request).then(new_response => {
-        //     actualizarCacheDinamico(DYNAMIC_CACHE,event.request,new_response);
-        // });
+    }
+    else{
         return res;
     }
+
 }
